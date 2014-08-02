967468792c23f75a3245a266c6192aeb7f524521
Connor Hindley <conn.hindley@gmail.com>
2014-08-02 00:29:21 -0400

    more CORS

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ app.use(function(req, res, next) {
 
 app.all('*', function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'X-Requested-With');
     next();
 });
